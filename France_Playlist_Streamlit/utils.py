f05093b26ec7d1e9d19b94f75bd219f8bdea01ca
ShriyaMehta111 <shriyamehta837@gmail.com>
2026-08-04 15:36:44 +0530

    Updated Files3

--- France_Playlist_Streamlit/utils.py
+++ France_Playlist_Streamlit/utils.py
@@ -1,12 +1,5 @@
 import streamlit as st
-from pathlib import Path
-import pandas as pd
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_PATH = BASE_DIR / "data" / "france_top50_cleaned.csv"
-
-def load_data():
-    return pd.read_csv(DATA_PATH)
 
 def footer():
 
